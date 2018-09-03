5c280c4724d32acf2a496660fb67a607a3c48567
afforsoftbd <affordsoft@gmail.com>
2018-09-04 04:38:41 +0600

    layout blades created

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <!-- Header -->
+  <head>
+    @include('layouts.partials.meta')
+
+    <!-- Favicon-->
+    <link rel="icon" href="{!! asset('favicon.png') !!}"/>
+
+    <title>@yield('title') Amar Neta || Connect with your workers</title>
+
+    @include('layouts.partials.styles')
+  </head>
+  <!-- #ENDS# Header -->
+
+  <body {!! Request::is('/')? 'class="index"':'' !!}>
+    
+  	@include('layouts.partials.loader')
+  	@include('layouts.partials.navigation')
+      
+    <!-- Content -->
+    @yield('content')
+    <!-- #ENDS# Content -->
+
+  	@include('layouts.partials.alerts')
+    @include('layouts.partials.scrolltotop')
+  	@include('layouts.partials.footer')
+    @include('layouts.partials.scripts')
+
+  </body>
+
+</html>
