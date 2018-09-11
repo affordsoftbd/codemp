17f42eb9ef15c2320ebfef930e77f9d61c7cba5e
bappy25 <bappychanting@outlook.com>
2018-09-11 16:53:48 +0600

    added status tabs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,58 @@
 @section('content')
 
 <h4>What's on your mind?</h4><hr>
-{!! Form::open() !!}	
-    <div class="md-form">
-    	{!! Form::textarea('additional_details', null, array('class'=>'editor')) !!}
-    </div>
-    <div class="text-center my-4">
-    	{!! Form::button('Update Status', array('type' => 'submit', 'class' =>'btn btn-danger btn-sm pull-right')) !!}
-    </div>
-{!! Form::close() !!}
 
- <div class="clearfix"></div>
+<!-- Nav tabs -->
+<ul class="nav nav-tabs md-tabs nav-justified">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#panel1" role="tab">
+        Update Status</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel2" role="tab">
+        Upload Images</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel3" role="tab">
+        Start Video</a>
+    </li>
+</ul>
+<!-- Tab panels -->
+<div class="tab-content card">
+    <!--Panel 1-->
+    <div class="tab-pane fade in show active" id="panel1" role="tabpanel">
+        {!! Form::open() !!}    
+            <div class="md-form">
+                {!! Form::textarea('additional_details', null, array('class'=>'editor')) !!}
+            </div>
+            <div class="text-center my-4">
+                {!! Form::button('Update Status', array('type' => 'submit', 'class' =>'btn btn-danger btn-sm pull-right')) !!}
+            </div>
+        {!! Form::close() !!}
+        <div class="clearfix"></div>
+    </div>
+    <!--/.Panel 1-->
+    <!--Panel 2-->
+    <div class="tab-pane fade" id="panel2" role="tabpanel">
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+    </div>
+    <!--/.Panel 2-->
+    <!--Panel 3-->
+    <div class="tab-pane fade" id="panel3" role="tabpanel">
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+    </div>
+    <!--/.Panel 3-->
+</div>
+
 
 
 <div class="card my-3">
