58a2140fd16573d3bd63323b8af91b722f7e3b49
Muhin <muhin.diu092@gmail.com>
2018-09-16 13:24:38 +0600

    Change route and sync local

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,26 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect('/home');
+})->name('welcome');
+Route::get('login','AuthController@login');
+Route::post('login','AuthController@postLogin');
+Route::get('logout','AuthController@logout');
+
+Route::get('/register/politician','AuthController@politicianRegister')->name('register.politician');
+Route::get('/register/public','AuthController@publicRegister')->name('register.public');
+/*Route::get('/register/politician', function () {
+    return view('auth.politician_register');
+})->name('register.politician');*/
+/*Route::get('/register/public', function () {
+    return view('auth.public_register');
+})->name('register.public');*/
+Route::post('save_user','AuthController@saveUser');
+
+// Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('district_by_division', 'CommonController@getDistrictByDivision');
+Route::post('thana_by_district', 'CommonController@getThanaByDistrict');
+Route::post('zip_by_thana', 'CommonController@getZipByThana');
