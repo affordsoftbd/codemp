6b50d5a9466bfdfe1104bea28515059e5ad718c4
afforsoftbd <affordsoft@gmail.com>
2018-09-23 21:05:52 +0600

    creating seeders

--- database/seeds/DivisionTableSeeder.php
+++ database/seeds/DivisionTableSeeder.php
@@ -11,6 +11,19 @@ class DivisionTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $divisions = [
+            ['division_name'=>'ঢাকা', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'ময়মনসিংহ', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'চট্টগ্রাম', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'খুলনা', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'বরিশাল', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'সিলেট', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'রংপুর', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")],
+            ['division_name'=>'রাজশাহী', 'created_at' => strftime("%Y-%m-%d %H:%M:%S"), 'updated_at' => strftime("%Y-%m-%d %H:%M:%S")]
+        ];
+
+        
+        DB::table("divisions")->insert($divisions);
     }
 }
