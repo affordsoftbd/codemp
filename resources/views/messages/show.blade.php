7f914b3555c70cf1a6b39c1738d7edbd29625508
afforsoftbd <affordsoft@gmail.com>
2018-11-09 17:20:36 +0600

    more changes

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -96,31 +96,6 @@
         <h5>অংশগ্রাহীরা</h5><hr>
     </div>
 
-    <!-- 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#centralModalSm">
-	  Launch demo modal
-	</button>
-	<div class="modal fade" id="centralModalSm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	    <div class="modal-dialog modal-lg" role="document">
-	      	<div class="modal-content">
-		        <div class="modal-header">
-		          <h4 class="modal-title w-100" id="myModalLabel">Modal title</h4>
-		          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		            <span aria-hidden="true">&times;</span>
-		          </button>
-		        </div>
-		        <div class="modal-body">
-		          ...
-		        </div>
-		        <div class="modal-footer">
-		          <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
-		          <button type="button" class="btn btn-primary btn-sm">Save changes</button>
-		        </div>
-	  		</div>
-		</div>
-	</div> 
-	-->
-
 </div>
 
 @endsection
