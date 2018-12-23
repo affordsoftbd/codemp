15071ed83f500aceb1ab6be261b1c7fd103009bf
afforsoftbd <affordsoft@gmail.com>
2018-12-23 23:35:56 +0600

    few updates

---
+++ resources/views/videos/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', "ভিডিও ||")
+
+@section('content')
+
+<h4 class="red-text">একটি নতুন ভিডিও তৈরি করুন</h4>
+<hr>
+
+<a class="btn btn-success waves-effect btn-sm" href="{{ route('videos.index') }}"><i class="fa fa-arrow-circle-left fa-sm pr-2" aria-hidden="true"></i>প্রত্যাবর্তন</a>
+
+@endsection
+
+
